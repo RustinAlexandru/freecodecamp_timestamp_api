aa00fcf74a833f91f305d527602a70c62fdd993c
Rustin Alexandru <rustinalexandru@gmail.com>
2017-06-09 10:27:44 +0000

    Completed app

---
+++ server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+const moment = require("moment");
+
+const app = express();
+
+app.get('/:query', function (req, res) {
+  const queryDateString = req.params.query;
+  
+  let unixtime, natural;
+  
+  const unixTime = parseInt(queryDateString);
+  
+  if (unixTime && unixTime >= 0) { // query is a unix number
+    unixtime = unixTime;
+    natural = moment.unix(unixTime).format("MMMM D, YYYY");
+  }
+  else if (isNaN(queryDateString) && moment(queryDateString, "MMMM, d YYYY").isValid()) { // query is not a number, check if natural date
+    unixtime = moment(queryDateString, "MMMM D, YYYY").format("X");
+    natural = queryDateString;
+ } else {
+    unixtime = null;
+    natural = null;
+ }
+ 
+  const obj = {
+    unixtime: unixtime,
+    natural: natural
+  };
+  
+  res.send(obj);
+  
+})
+
+app.listen(8080);
